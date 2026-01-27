b79f0c38c0908fe49e626dd14b13102d3a7e1a42
sgestri1 <112642645+sgestri1@users.noreply.github.com>
2026-01-27 12:14:37 -0500

    Update scripts.js
    Edited variables

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-//Code for filter buttons
+//Code for filter buttons. Used https://www.w3schools.com/howto/howto_js_filter_elements.asp as a reference.
 filterSelection("all")
 function filterSelection(f) {
   var x, i;
@@ -14,13 +14,13 @@ function filterSelection(f) {
   if (f == "all") f = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(f) > -1) w3AddClass(x[i], "show");
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(f) > -1) addClass(x[i], "show");
   }
 }
 
 // Show filtered elements
-function w3AddClass(element, name) {
+function addClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -32,7 +32,7 @@ function w3AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function removeClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
