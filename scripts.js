e8145576675369989efcc6a2cf5103550d10958f
sgestri1 <112642645+sgestri1@users.noreply.github.com>
2026-01-27 12:26:13 -0500

    Update scripts.js
    Variables

--- scripts.js
+++ scripts.js
@@ -9,42 +9,42 @@ function topFunction() {
 //Code for filter buttons. Used https://www.w3schools.com/howto/howto_js_filter_elements.asp as a reference.
 filterSelection("all")
 function filterSelection(f) {
-  var x, i;
-  x = document.getElementsByClassName("col-sm-12");
+  var y, i;
+  y = document.getElementsByClassName("col-sm-12");
   if (f == "all") f = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(f) > -1) addClass(x[i], "show");
+  // Code for adding the "show" class to the filtered elements, and remove the "show" class from the elements being filtered out
+  for (i = 0; i < y.length; i++) {
+    removeClass(y[i], "show");
+    if (y[i].className.indexOf(f) > -1) addClass(y[i], "show");
   }
 }
 
-// Show filtered elements
+// Code for showing the filtered elements (those that are selected)
 function addClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+  var i, array1, array2;
+  array1 = element.className.split(" ");
+  array2 = name.split(" ");
+  for (i = 0; i < array2.length; i++) {
+    if (array1.indexOf(array2[i]) == -1) {
+      element.className += " " + array2[i];
     }
   }
 }
 
-// Hide elements that are not selected
+// Code for hiding elements that are filtered out (not selected)
 function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+  var i, array1, array2;
+  array1 = element.className.split(" ");
+  array2 = name.split(" ");
+  for (i = 0; i < array2.length; i++) {
+    while (array1.indexOf(array2[i]) > -1) {
+      array1.splice(array1.indexOf(array2[i]), 1);
     }
   }
-  element.className = arr1.join(" ");
+  element.className = array1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
+// Code for adding the active class to the current button
 var btnContainer = document.getElementById("filterContainer");
 var btns = btnContainer.getElementsByClassName("btnFilter");
 for (var i = 0; i < btns.length; i++) {
